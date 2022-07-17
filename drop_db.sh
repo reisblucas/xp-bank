b3f53b18f24c720f6875fbf1ddcf931c9f97f2d9
Lucas B. Reis <lucasb.reis@hotmail.com>
2022-07-17 00:14:15 -0300

    refactor(package): renew scripts in package.json and try another bank using mysql

---
+++ drop_db.sh
@@ -0,0 +1,17 @@
+#!/bash/sh
+
+sleep 1s
+
+sqlFile=$(<sql_file_name.txt)
+dbName=$(<dbname.txt)
+
+SQL_PATH=${PWD}
+mysql -e "
+DROP SCHEMA IF EXISTS ${dbName};
+CREATE SCHEMA IF NOT EXISTS ${dbName};
+USE ${dbName};
+
+source ${SQL_PATH}/${sqlFile};
+"
+
+echo "${dbName} recreated using ${sqlFile} is done!"
