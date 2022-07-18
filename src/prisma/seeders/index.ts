46b53295c763a760fba1820ecfb7c045d3acb0c4
Lucas B. Reis <lucasb.reis@hotmail.com>
2022-07-18 15:38:38 -0300

    refactor(seeders): abstract UserPersonalData

--- src/prisma/seeders/index.ts
+++ src/prisma/seeders/index.ts
@@ -1,61 +1,14 @@
 import { PrismaClient } from '@prisma/client';
-import security from '../../utils/security';
-import birthDate from '../../utils/birth_date';
-import UsersPersonalData from '../../data/seeds/UsersPersonalData.json';
-import RGs from '../../data/seeds/RG.json';
-import CPFs from '../../data/seeds/CPF.json';
-import GendersRelation from '../../data/seeds/GendersRelation.json';
-import Addresses from '../../data/seeds/Addresses.json';
-import AccessHRelations from '../../data/seeds/AccessHRelations.json';
 import Gender from './Genders';
 import Platform from './Platforms';
+import UserPersonalData from './UsersPersonalDatas';
 
 const prisma = new PrismaClient();
 
 function main() {
   Gender(prisma);
   Platform(prisma);
-
-  // seed users: UserLogin -> AccountsBalance -> Addresses | when user signup
-  UsersPersonalData.map(async (user, i) => prisma.usersLogin.create({
-    data: {
-      email: user.email,
-      password: security.encryptAndHash(user.password),
-      salt: security.salt.dynamic,
-      AccountsBalance: {
-        create: {
-          balance: 0,
-        },
-      },
-      PersonalDatas: {
-        create: {
-          first_name: user.first_name,
-          last_name: user.last_name,
-          birth_date: birthDate.serialize(user.birth_date), // yyyy/mm/dd -> yyyy-mm-dd
-          rg: RGs[i],
-          cpf: CPFs[i],
-          Genders_id: Number(GendersRelation[i].userRelation),
-          Addresses: {
-            create: {
-              postal_code: Addresses[i].cep,
-              logradouro: Addresses[i].logradouro,
-              complement: Addresses[i].complemento,
-              number: Addresses[i].numero,
-              district: Addresses[i].bairro,
-              city: Addresses[i].cidade,
-              state: Addresses[i].estado,
-              state_code: Addresses[i].estadoSigla,
-            },
-          },
-        },
-      },
-      AccessHistory: {
-        create: {
-          Platform_id: Number(AccessHRelations[i]),
-        },
-      },
-    },
-  }));
+  UserPersonalData(prisma);
 }
 
 // // when main is async
