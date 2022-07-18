e097d418acb346366af06fbea35a399131b2cdd6
Lucas B. Reis <lucasb.reis@hotmail.com>
2022-07-18 07:53:43 -0300

    feat(security): add encrypt with salt to hash my infos in db password column

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import app from './app';
 import 'dotenv/config';
+import security from '@utils/security';
 
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => console.log(
   `I'm listening you on port ${PORT}, my young padawan`,
 ));
+
+console.log('encrypter: \n', security.encrypter('lucas'), '\n');
+console.log('hasher: \n', security.hasher('lucas'), '\n');
+console.log('encryptAndHash: \n', security.encryptAndHash('lucas'), '\n');
