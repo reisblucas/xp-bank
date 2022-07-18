75e3756371730ffcc162f671523c6bc94e0ffe84
Lucas B. Reis <lucasb.reis@hotmail.com>
2022-07-18 16:36:13 -0300

    refactor(models): stocks: set cpnj and dash_cnpj to null

--- src/prisma/seeders/Stocks/index.ts
+++ src/prisma/seeders/Stocks/index.ts
@@ -4,8 +4,14 @@ import Stocks from '../../../data/seeds/Stocks.json';
 const sStocks = (prisma: PrismaClient) => Stocks
   .map(async (stock) => prisma.stocks.create({
     data: {
-      name: stock.nm_empresa,
       symbol: stock.cd_acao_rdz,
+      name: stock.nm_empresa,
+      economic_sector: stock.setor_economico,
+      sub_sector: stock.subsetor,
+      segment: stock.segmento,
+      segment_b3: stock.segmento_b3,
+      cnpj: stock.vl_cnpj,
+      dash_cnpj: stock.tx_cnpj,
     },
   }));
 
