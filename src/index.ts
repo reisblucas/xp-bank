12c2a586dd2066958d7a3a672b5f67f051ab3306
Lucas B. Reis <lucasb.reis@hotmail.com>
2022-07-18 22:36:21 -0300

    chore(remove): console log and crypt at index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import security from '@utils/security';
 import app from './app';
 import 'dotenv/config';
 
@@ -11,10 +10,3 @@ app.listen(PORT, () => console.log(
 // console.log('encrypter: \n', security.encrypter('lucas'), '\n');
 // console.log('hasher: \n', security.hasher('lucas'), '\n');
 // console.log('encryptAndHash: \n', security.encryptAndHash('lucas'), '\n');
-
-// const local_salt = process.env.SALT as string;
-const pwdDb = 'pwssR+spUuulNHUHv0A25s2Q+FQLm5KRIgTACyCg4NQ=';
-const saltDb = 'fQzGumqpEIi7PlrvzTO+1JSLX1M8c+EW5f8VvydLrXk=';
-
-console.log('if info hashed', security.validateHash(pwdDb, saltDb));
-console.log('pwd encrypted ', security.validateHash('lucas', saltDb));
