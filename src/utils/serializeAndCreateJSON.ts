b96f61ae176c80446394b63affe59824ac6ade9e
Lucas B. Reis <lucasb.reis@hotmail.com>
2022-07-19 01:37:46 -0300

    feat(seed): serializedInfoMoney: create new file with infos serialized and provided by InfoMoney

--- src/utils/serializeAndCreateJSON.ts
+++ src/utils/serializeAndCreateJSON.ts
@@ -1,28 +1,30 @@
 import { writeFile } from 'fs';
 import ENoMagicNumbers from 'src/__interfaces__/nomagicnum.enum';
-import UnserializedOverview from '../data/seeds/UnserializedOverview.json';
+import UnserializedFromInfoMoney from '../data/seeds/UnserializedFromInfoMoney.json';
 
-// pos | key
-// 1 -> name
-// 2 -> class (ON, PN or UNIT)
-// 3 -> last price selled
-// 4 -> max
-// 5 -> min
-// 6 -> var
-// 7 -> var%
-// 8 -> vol ('x'M -> Millions)
-// 9 -> hour
+// pos. | key
+// 1 -> ticker
+// 2 -> lastSell
+// 3 -> max
+// 4 -> min
+// 5 -> varDay
+// 6 -> varSem
+// 7 -> varYear%
+// 8 -> var12m
+// 9 -> vol ('x'M -> Millions)
+// 10 -> date
 
 interface IStocksOverview {
-  name: string;
-  class: string;
+  ticker: string;
+  date: string;
   lastSell: number;
   max: number;
   min: number;
-  var: string;
-  varPercent: string;
+  varDay: number;
+  varSem: number;
+  varYear: number;
+  var12m: number;
   vol: number;
-  lastCheck: string;
 }
 
 // serialize to number and turn into million
@@ -35,29 +37,32 @@ const serializeVolField = (vol: string): number => {
   return Math.floor(Number(sliced) * +MILLION);
 };
 
-const serializeStocksInfo = () => UnserializedOverview
+const concatYear = (date: string): string => `${date}/${new Date().getFullYear()}`;
+
+const serializeStocksInfo = (): IStocksOverview[] => UnserializedFromInfoMoney
   .map((str: string) => {
     const key = {} as IStocksOverview;
 
     str.split(',')
       .forEach((field: string, i: number) => {
         const dataTrimmed = field.trim();
-        if (i === 0) key.name = dataTrimmed;
-        if (i === 1) key.class = dataTrimmed;
+        if (i === 0) key.ticker = dataTrimmed;
+        if (i === 1) key.date = concatYear(dataTrimmed);
         if (i === 2) key.lastSell = Number(dataTrimmed);
         if (i === 3) key.max = Number(dataTrimmed);
-        if (i === 4) key.min = Number(dataTrimmed);
-        if (i === 5) key.var = dataTrimmed;
-        if (i === 6) key.varPercent = dataTrimmed;
-        if (i === 7) key.vol = serializeVolField(dataTrimmed);
-        if (i === 8) key.lastCheck = dataTrimmed;
+        if (i === 4) key.max = Number(dataTrimmed);
+        if (i === 5) key.min = Number(dataTrimmed);
+        if (i === 6) key.varDay = Number(dataTrimmed);
+        if (i === 7) key.varSem = Number(dataTrimmed);
+        if (i === 8) key.varYear = Number(dataTrimmed);
+        if (i === 9) key.vol = serializeVolField(dataTrimmed);
       });
     return key;
   });
 
-const serialized = serializeStocksInfo();
-
 const createNewFile = (path: string) => {
+  const serialized = serializeStocksInfo();
+
   writeFile(path, JSON.stringify(serialized, null), (err) => {
     if (err) console.log('Error when write file: ', err);
     console.log('File created succesfully!');
