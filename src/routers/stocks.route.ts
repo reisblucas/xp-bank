266f410b410abb79bd5e3496d02e5b11251978cb
Lucas B. Reis <lucasb.reis@hotmail.com>
2022-07-21 07:46:08 -0300

    feat(routers): add AUTH middleware in all routes, now all routes has been protected

--- src/routers/stocks.route.ts
+++ src/routers/stocks.route.ts
@@ -1,15 +1,16 @@
 import { IRouter, Router } from 'express';
 import stocksController from '@controllers/stocks.controller';
+import mid from '@middlewares/index';
 
 require('express-async-errors');
 
 const stocksRoute: IRouter = Router();
 
-stocksRoute.get('/o/stocks', stocksController.getAllStocks);
-// stocksRoute.get('/i/stocks/:ticker', stocksController.getAllTickers));
-stocksRoute.get('/i/companies', stocksController.getAllCompaniesInfo);
-stocksRoute.get('/i/companies/:ticker', stocksController.getCompanyInfo);
-stocksRoute.get('/i/tickers', stocksController.getAllTickers);
-stocksRoute.get('/o/:ticker', stocksController.getTickerOverview);
+stocksRoute.get('/o/stocks', mid.auth, stocksController.getAllStocks);
+// stocksRoute.get('/i/stocks/:ticker', mid.auth, stocksController.getAllTickers));
+stocksRoute.get('/i/companies', mid.auth, stocksController.getAllCompaniesInfo);
+stocksRoute.get('/i/companies/:ticker', mid.auth, stocksController.getCompanyInfo);
+stocksRoute.get('/i/tickers', mid.auth, stocksController.getAllTickers);
+stocksRoute.get('/o/:ticker', mid.auth, stocksController.getTickerOverview);
 
 export default stocksRoute;
