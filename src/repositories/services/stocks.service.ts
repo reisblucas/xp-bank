a2c44d0d5775784b4afa73af70c72fdf0f32186e
Lucas B. Reis <lucasb.reis@hotmail.com>
2022-07-20 21:29:06 -0300

    feat(services): add STOCKS services
    Services to:
     - getAllStocks
     - getTickerInfos
     - getAllCompaniesInfo
     - getAllTickers
     -
    getCompanyInfo

--- src/repositories/services/stocks.service.ts
+++ src/repositories/services/stocks.service.ts
@@ -3,9 +3,55 @@ import { PrismaClient } from '@prisma/client';
 export default class StocksService {
   constructor(private prisma = new PrismaClient()) {}
 
-  public getAll = async () => this.prisma.tickers.findMany(({
+  public getAllStocks = async () => this.prisma.tickers.findMany(({
     include: {
       FSExchangeOverview: {},
     },
   }));
+
+  public getAllTickers = async () => this.prisma.stocks.findMany({
+    select: {
+      id: true,
+      name: true,
+      symbol: true,
+      Tickers: {
+        select: {
+          ticker: true,
+        },
+      },
+    },
+  });
+
+  public getTickerInfos = async (ticker: string) => this.prisma.tickers.findFirst({
+    where: {
+      ticker,
+    },
+    include: {
+      FSExchangeOverview: {},
+    },
+  });
+
+  public getAllCompaniesInfo = async () => this.prisma.stocks.findMany({
+    include: {
+      Tickers: {},
+    },
+  });
+
+  public getCompanyInfo = async (ticker: string) => this.prisma.stocks.findFirst({
+    include: {
+      Tickers: {
+        select: {
+          id: true,
+          ticker: true,
+        },
+      },
+    },
+    where: {
+      Tickers: {
+        some: {
+          ticker,
+        },
+      },
+    },
+  });
 }
