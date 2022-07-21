971b8c7f6ac19e5d603d7965c753acaa21485ede
Lucas B. Reis <lucasb.reis@hotmail.com>
2022-07-21 01:19:01 -0300

    build(dependencies): change express-rescue to express-async-errors
    because it's more scalable

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+import signUpRoute from './login.route';
 import stocksRoute from './stocks.route';
 
 const routers = Router();
 
+routers.use('/signup', signUpRoute);
+// routers.use('/signin', siginInRoute);
 routers.use('/asset', stocksRoute);
 
 export default routers;
--- src/routers/stocks.route.ts
+++ src/routers/stocks.route.ts
@@ -1,14 +1,15 @@
 import { IRouter, Router } from 'express';
 import stocksController from '@controllers/stocks.controller';
-import rescue from 'express-rescue';
+
+require('express-async-errors');
 
 const stocksRoute: IRouter = Router();
 
-stocksRoute.get('/o/stocks', rescue(stocksController.getAllStocks));
-// stocksRoute.get('/i/stocks/:ticker', rescue(stocksController.getAllTickers));
-stocksRoute.get('/i/companies', rescue(stocksController.getAllCompaniesInfo));
-stocksRoute.get('/i/companies/:ticker', rescue(stocksController.getCompanyInfo));
-stocksRoute.get('/i/tickers', rescue(stocksController.getAllTickers));
-stocksRoute.get('/o/:ticker', rescue(stocksController.getTickerOverview));
+stocksRoute.get('/o/stocks', stocksController.getAllStocks);
+// stocksRoute.get('/i/stocks/:ticker', stocksController.getAllTickers));
+stocksRoute.get('/i/companies', stocksController.getAllCompaniesInfo);
+stocksRoute.get('/i/companies/:ticker', stocksController.getCompanyInfo);
+stocksRoute.get('/i/tickers', stocksController.getAllTickers);
+stocksRoute.get('/o/:ticker', stocksController.getTickerOverview);
 
 export default stocksRoute;
