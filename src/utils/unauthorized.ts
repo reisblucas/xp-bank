1738d4b285b2cf2ee0d949bb1f1061978b71517e
Lucas B. Reis <lucasb.reis@hotmail.com>
2022-07-22 03:06:18 -0300

    chore(unauthorized): remove commented code

--- src/utils/unauthorized.ts
+++ src/utils/unauthorized.ts
@@ -2,8 +2,6 @@ import { Users } from '@prisma/client';
 import { StatusCodes, ReasonPhrases } from 'http-status-codes';
 import HttpException from './HttpException';
 
-// require('express-async-errors');
-
 const unauthorized = (data: Users | null | boolean) => {
   if (!data) {
     throw new HttpException(StatusCodes.UNAUTHORIZED, ReasonPhrases.UNAUTHORIZED);
