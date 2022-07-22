55a7ea17e3fc01f1ffab685fc5d45c14a4126143
Lucas B. Reis <lucasb.reis@hotmail.com>
2022-07-22 02:11:03 -0300

    feat(controllers): add method GET_BALANCE in USERS controller

--- src/repositories/controllers/users.controller.ts
+++ src/repositories/controllers/users.controller.ts
@@ -1,5 +1,6 @@
 import { IUserSignIn, IUserSignUp } from '@interfaces/users.interface';
 import UsersService from '@services/users.service';
+import HttpException from '@utils/HttpException';
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
@@ -17,7 +18,18 @@ class UsersController {
   public signIn = async (req: Request, res: Response) => {
     const isSigned = await this.service.signIn(req.body as IUserSignIn);
 
-    res.status(StatusCodes.CREATED).json(isSigned);
+    res.status(StatusCodes.OK).json(isSigned);
+  };
+
+  public getBalance = async (req: Request, res: Response) => {
+    const userId = res.locals.provider.id as number;
+    if (!userId) {
+      throw new HttpException(StatusCodes.BAD_REQUEST, 'Provider does not exists');
+    }
+
+    const balance = await this.service.getBalance(userId);
+
+    res.status(StatusCodes.OK).json(balance);
   };
 }
 
