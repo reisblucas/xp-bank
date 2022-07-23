eaa3c7fc306dff12d495b262768a8aa955098716
Lucas B. Reis <lucasb.reis@hotmail.com>
2022-07-23 05:59:45 -0300

    chore(seeders): update PLATFORMS seeder

--- src/prisma/seeders/Platforms/index.ts
+++ src/prisma/seeders/Platforms/index.ts
@@ -4,7 +4,8 @@ import Platforms from '../../../data/seeds/Platforms.json';
 
 const sPlatforms = (prisma: PrismaClient) => Platforms.map(async (p) => prisma.platform.create({
   data: {
-    name: p,
+    id: p.id,
+    name: p.name,
   },
 }));
 
