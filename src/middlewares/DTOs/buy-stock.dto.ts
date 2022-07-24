a219bc16a4419f5c64e9a41b0dab6090515d39ca
Lucas B. Reis <lucasb.reis@hotmail.com>
2022-07-24 05:27:58 -0300

    refactor(jwt): because of the JWT is useless to send the userId through the req body

--- src/middlewares/DTOs/buy-stock.dto.ts
+++ src/middlewares/DTOs/buy-stock.dto.ts
@@ -2,8 +2,6 @@ import { z } from 'zod';
 
 const BulkBuySellStocksDTO = z.object({
   body: z.object({
-    userId: z.number({ required_error: 'User Id is required' })
-      .int(),
     tickerId: z.number({ required_error: 'User Id is required' })
       .int(),
     quantity: z.number({ required_error: 'User Id is required' })
--- src/routers/authenticated/account.route.ts
+++ src/routers/authenticated/account.route.ts
@@ -1,5 +1,5 @@
 import accountsController from '@controllers/accounts.controller';
-import BuySellDTO from '@middlewares/DTOs/account.dto';
+import DepositWithdrawDTO from '@middlewares/DTOs/account.dto';
 import validate from '@middlewares/validate.mid';
 import { Router } from 'express';
 
@@ -7,7 +7,7 @@ const accountRoute = Router();
 
 accountRoute.get('/balance', accountsController.getBalance);
 accountRoute.get('/statement', accountsController.getStatement);
-accountRoute.post('/deposit', validate(BuySellDTO), accountsController.deposit);
-accountRoute.post('/withdraw', validate(BuySellDTO), accountsController.withdraw);
+accountRoute.post('/deposit', validate(DepositWithdrawDTO), accountsController.deposit);
+accountRoute.post('/withdraw', validate(DepositWithdrawDTO), accountsController.withdraw);
 
 export default accountRoute;
