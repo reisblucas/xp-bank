799ed3eb33fea9ec3fa5f7c3ca2ca939857bdd69
Lucas B. Reis <lucasb.reis@hotmail.com>
2022-07-24 08:57:48 -0300

    chore(swagger): set swagger to initial the root url '/'

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ dotenv.config();
 const app: Express = express();
 app.use(cors());
 app.use(express.json());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(MySwagger));
+app.use('/', swaggerUi.serve, swaggerUi.setup(MySwagger));
 
 app.use(unauthenticatedRouters);
 app.use(authenticatedRouters);
