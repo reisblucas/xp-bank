3de3423cc7a747cf5fb93fb7d4bb7cba8830aaaa
Lucas B. Reis <lucasb.reis@hotmail.com>
2022-07-24 02:15:54 -0300

    feat(swagger): initial swagger structure

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,19 @@
 import express, { Express } from 'express';
 import * as dotenv from 'dotenv';
-import { absolutePath } from 'swagger-ui-dist';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
 import ErrorHandler from './middlewares/ErrorHandler';
 import authenticatedRouters from './routers/authenticated';
 import unauthenticatedRouters from './routers/public';
-import swaggerUi from 'swagger-ui-express';
+import MySwagger from './__swagger__/swagger.json';
 
 dotenv.config();
 
 const app: Express = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(absolutePath()));
+// app.use(express.static(absolutePath()));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(MySwagger));
 
 app.use(unauthenticatedRouters);
 app.use(authenticatedRouters);
