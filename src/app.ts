dd86c82d4b43a63bdb9956529117b3d64649d0f7
Lucas B. Reis <lucasb.reis@hotmail.com>
2022-07-23 22:20:34 -0300

    build(dependencies): add swagger-ui and swagger-ui-dist with @types

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,19 @@
 import express, { Express } from 'express';
 import * as dotenv from 'dotenv';
+// import SwaggerUI from 'swagger-ui';
+import { absolutePath } from 'swagger-ui-dist';
 import ErrorHandler from './middlewares/ErrorHandler';
 import authenticatedRouters from './routers/authenticated';
 import unauthenticatedRouters from './routers/public';
 
 dotenv.config();
 
+// SwaggerUI({
+//   dom_id: 'localhost:3000',
+// }).;
+
 const app: Express = express();
+app.use(express.static(absolutePath()));
 app.use(express.json());
 
 app.use(unauthenticatedRouters);
